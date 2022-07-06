10f04d618f944c712b00c512cc990f4e90dd2b5b
hjkim0514 <97289374+hjkim0514@users.noreply.github.com>
2022-07-06 15:39:59 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+(function(sentence){
+    let arrrayOfWords = sentence.split(" ");
+    console.log(arrrayOfWords)
+}) ("Hello my name is Cheetah!");
+
+let creatingLoops=(x)=>{
+    for(let i=0; i<x; i++)
+    {
+        console.log(i);
+    }
+}
+creatingLoops(10);
